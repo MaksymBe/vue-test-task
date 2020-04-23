1a8056673c5391599cabcbe38534a036cbf53dac
Maksym Berezovskyi <maksymbe@interlink-ua.com>
2020-04-23 13:55:43 +0300

    Initial setup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+import './plugins/ant-design-vue.js';
+import store from './store'
 
-Vue.config.productionTip = false
+Vue.use(VueAxios, axios);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
